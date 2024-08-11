5dc0efb4ce846cf3ed4016cfa7ef1aeb6f31bb62
r7kamura <r7kamura@gmail.com>
2024-08-11 10:49:41 +0900

    Refactor ProcessedSourceBuilder.call

--- lib/rubocop/erb/processed_source_builder.rb
+++ lib/rubocop/erb/processed_source_builder.rb
@@ -2,7 +2,7 @@
 
 module RuboCop
   module Erb
-    module ProcessedSourceBuilder
+    class ProcessedSourceBuilder
       class << self
         # Creates a new ProcessedSource, inheriting state from a donor.
         #
@@ -13,25 +13,41 @@ module RuboCop
           code:,
           processed_source:
         )
-          supports_prism = processed_source.respond_to?(:parser_engine)
-          new_processed_source =
-            if supports_prism
-              ::RuboCop::ProcessedSource.new(
-                code,
-                processed_source.ruby_version,
-                processed_source.path,
-                parser_engine: processed_source.parser_engine
-              )
-            else
-              ::RuboCop::ProcessedSource.new(
-                code,
-                processed_source.ruby_version,
-                processed_source.path
-              )
-            end
-          new_processed_source.config = processed_source.config
-          new_processed_source.registry = processed_source.registry
-          new_processed_source
+          new(
+            code: code,
+            processed_source: processed_source
+          ).call
+        end
+      end
+
+      def initialize(
+        code:,
+        processed_source:
+      )
+        @code = code
+        @processed_source = processed_source
+      end
+
+      def call
+        new_processed_source = ::RuboCop::ProcessedSource.new(
+          @code,
+          @processed_source.ruby_version,
+          @processed_source.path,
+          **keyword_arguments
+        )
+        new_processed_source.config = @processed_source.config
+        new_processed_source.registry = @processed_source.registry
+        new_processed_source
+      end
+
+      private
+
+      # @return [Hash]
+      def keyword_arguments
+        if @processed_source.respond_to?(:parser_engine)
+          { parser_engine: @processed_source.parser_engine }
+        else
+          {}
         end
       end
     end
